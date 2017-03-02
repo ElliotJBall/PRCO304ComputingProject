174d1c6318072f388b9b439f2d96d8fa94c4e97f
Elliot Ball - Desktop <elliot.ball@students.plymouth.ac.uk>
2017-03-02 15:17:01 +0000

    Fixed git root error

--- app/src/main/java/com/example/elliot/automatedorderingsystem/LoginActivity.java
+++ app/src/main/java/com/example/elliot/automatedorderingsystem/LoginActivity.java
@@ -25,7 +25,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         btnContinueAsGuest = (Button) findViewById(R.id.btnContinueAsGuest);
         btnContinueAsGuest.setOnClickListener(this);
 
-        MongoClient
+
     }
 
     @Override
